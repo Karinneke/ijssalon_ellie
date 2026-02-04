f5422f61cf74016cf551ed093068ea9ae0066b1a
Karin Besten <kbesten@zonnet.nl>
2026-02-04 11:18:49 +0100

    bestanden boekhouding en presenteer toegevoegd

--- presentatie.py
+++ presentatie.py
@@ -2,4 +2,4 @@ def presenteer(data_bron, totaal):
     for item, bedrag in data_bron.items():
         print(f"{item} : {bedrag} euro")
     print("==========================")
-    print(f"totaal : {totaal} euro")
+    print(f"Totaal : {totaal} euro")
